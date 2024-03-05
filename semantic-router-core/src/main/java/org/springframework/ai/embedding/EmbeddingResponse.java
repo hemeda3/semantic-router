e36110c710f148523cab906eaea55668c65dc46d
ahmed <hemeda2@gmail.com>
2024-03-05 17:52:13 -0500

    first commit

---
+++ semantic-router-core/src/main/java/org/springframework/ai/embedding/EmbeddingResponse.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.embedding;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.ai.model.ModelResponse;
+import org.springframework.util.Assert;
+
+/**
+ * Embedding response object.
+ */
+public class EmbeddingResponse implements ModelResponse<Embedding> {
+
+	/**
+	 * Embedding data.
+	 */
+	private List<Embedding> embeddings;
+
+	/**
+	 * Embedding metadata.
+	 */
+	private EmbeddingResponseMetadata metadata = new EmbeddingResponseMetadata();
+
+	/**
+	 * Creates a new {@link EmbeddingResponse} instance with empty metadata.
+	 * @param embeddings the embedding data.
+	 */
+	public EmbeddingResponse(List<Embedding> embeddings) {
+		this(embeddings, new EmbeddingResponseMetadata());
+	}
+
+	/**
+	 * Creates a new {@link EmbeddingResponse} instance.
+	 * @param embeddings the embedding data.
+	 * @param metadata the embedding metadata.
+	 */
+	public EmbeddingResponse(List<Embedding> embeddings, EmbeddingResponseMetadata metadata) {
+		this.embeddings = embeddings;
+		this.metadata = metadata;
+	}
+
+	/**
+	 * @return Get the embedding metadata.
+	 */
+	public EmbeddingResponseMetadata getMetadata() {
+		return metadata;
+	}
+
+	@Override
+	public Embedding getResult() {
+		Assert.notEmpty(embeddings, "No embedding data available.");
+		return embeddings.get(0);
+	}
+
+	/**
+	 * @return Get the embedding data.
+	 */
+	@Override
+	public List<Embedding> getResults() {
+		return embeddings;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		EmbeddingResponse that = (EmbeddingResponse) o;
+		return Objects.equals(embeddings, that.embeddings) && Objects.equals(metadata, that.metadata);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(embeddings, metadata);
+	}
+
+	@Override
+	public String toString() {
+		return "EmbeddingResult{" + "data=" + embeddings + ", metadata=" + metadata + '}';
+	}
+
+}
---
+++ semantic-router-core/src/main/java/org/springframework/ai/vectorstore/VectorStore.java
@@ -0,0 +1,58 @@
+package org.springframework.ai.vectorstore;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.document.DocumentWriter;
+
+/**
+ * The {@code VectorStore} interface defines the operations for managing and querying
+ * documents in a vector database. It extends {@link DocumentWriter} to support document
+ * writing operations. Vector databases are specialized for AI applications, performing
+ * similarity searches based on vector representations of data rather than exact matches.
+ * This interface allows for adding, deleting, and searching documents based on their
+ * similarity to a given query.
+ */
+public interface VectorStore extends DocumentWriter {
+
+	/**
+	 * Adds list of {@link Document}s to the vector store.
+	 * @param documents the list of documents to store. Throws an exception if the
+	 * underlying provider checks for duplicate IDs.
+	 */
+	void add(List<Document> documents);
+
+	@Override
+	default void accept(List<Document> documents) {
+		add(documents);
+	}
+
+	/**
+	 * Deletes documents from the vector store.
+	 * @param idList list of document ids for which documents will be removed.
+	 * @return
+	 */
+	Optional<Boolean> delete(List<String> idList);
+
+	/**
+	 * Retrieves documents by query embedding similarity and metadata filters to retrieve
+	 * exactly the number of nearest-neighbor results that match the request criteria.
+	 * @param request Search request for set search parameters, such as the query text,
+	 * topK, similarity threshold and metadata filter expressions.
+	 * @return Returns documents th match the query request conditions.
+	 */
+	List<Document> similaritySearch(SearchRequest request);
+
+	/**
+	 * Retrieves documents by query embedding similarity using the default
+	 * {@link SearchRequest}'s' search criteria.
+	 * @param query Text to use for embedding similarity comparison.
+	 * @return Returns a list of documents that have embeddings similar to the query text
+	 * embedding.
+	 */
+	default List<Document> similaritySearch(String query) {
+		return this.similaritySearch(SearchRequest.query(query));
+	}
+
+}
